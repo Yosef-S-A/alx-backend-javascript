0286cca1ba07afd4bf6bffa5b31afc8a68bf16a1
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-24 17:34:09 -0500

    Added documentation

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * print in STDOUT the string argument
+ * @author Yosef Samuel
+ * @param {string} str - string to be printed
+ */
 module.exports = function displayMessage (str) {
   console.log(str);
 };
