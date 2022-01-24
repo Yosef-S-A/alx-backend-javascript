7b253f90f555e003f3acf7cea79bc7eb3da208f4
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-24 18:09:42 -0500

    Create 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+/*
+ * Using Process stdin
+ * @author Yosef Samuel
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', function () {
+  const name = process.stdin.read();
+  process.stdout.write('Your name is: ' + name);
+});
+
+process.stdin.on('end', function () {
+  process.stdout.write('This important software is now closing\n');
+});
