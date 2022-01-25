37699815480cd749622293134d43deef1eb19a3d
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-25 11:58:56 -0500

    added a checker for empty line (at the end)

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -12,7 +12,9 @@ function countStudents (path) {
     const fields = {};
     const students = data.split('\n').map((student) => student.split(','));
     students.shift();
-    //    students.pop();
+    if (students[students.length - 1][0] === '') {
+      students.pop();
+    }
     console.log(`Number of students: ${students.length}`);
 
     students.forEach((student) => {
@@ -22,9 +24,7 @@ function countStudents (path) {
       fields[student[3]].push(student[0]);
     });
     Object.keys(fields).forEach((key) => {
-      if (key !== 'undefined') {
-        console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
-      }
+      console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
     });
   } catch (err) {
     throw new Error('Cannot load the database');
