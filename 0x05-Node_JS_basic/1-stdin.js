0bfbcb95ef5def9116f335fb23bde96ea9185476
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-25 10:59:19 -0500

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,10 +6,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', function () {
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write('Your name is: ' + name);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
