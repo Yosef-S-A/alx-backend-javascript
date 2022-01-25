81b4e3711341b7e59b01c4156433d9095cf82614
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-25 12:45:21 -0500

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,11 @@
+const fs = require('fs');
+
 /*
  * Reading a file synchronously with Node JS
  * @author Yosef Samuel
  * @param {string} path
- * @return {void}
+ * return {void}
  */
-const fs = require('fs');
-
 function countStudents (path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
