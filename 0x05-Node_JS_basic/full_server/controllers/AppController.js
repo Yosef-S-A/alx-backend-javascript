4b3ef0345639ce9e7effc4ebb61267f1c4f6f209
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-25 15:15:14 -0500

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,12 @@
+  
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
+© 2022 GitHub, Inc.
+Terms
+Privacy
 
