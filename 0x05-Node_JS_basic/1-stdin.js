f9454dd307c032368710b36d5067982edc86bb6c
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-25 11:01:41 -0500

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,8 @@
  * @author Yosef Samuel
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', function () {
+
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
