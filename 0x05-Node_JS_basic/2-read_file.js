0cf372debc2ead05d047d38676f704fc186fc9ae
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-24 19:24:09 -0500

    Create 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+/*
+ * Reading a file synchronously with Node JS
+ * @author Yosef Samuel
+ * @param {string} path
+ * @return {void}
+ */
+const fs = require('fs');
+
+module.exports = function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const fields = {};
+    const students = data.split('\n').map((student) => student.split(','));
+    students.shift();
+    students.pop();
+    console.log(`Number of students: ${students.length}`);
+
+    students.forEach((student) => {
+      if (!fields[student[3]]) fields[student[3]] = [];
+      fields[student[3]].push(student[0]);
+    });
+    Object.keys(fields).forEach((key) => {
+      console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
