73ce6f5b1c8d108fb1b479ee2747aa2db8e7cbc1
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-27 08:33:58 -0500

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,33 +1,43 @@
+/*
+* @author Yosef Samuel
+*/
 const fs = require('fs');
 
-function readDatabase(path) {
-  const promise = (resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, dataDb) => {
-      if (err) reject(Error('Cannot load the database'));
-      if (dataDb) {
-        const inFields = {}; // {field: {counter: # of students, students: [list of students]}}
-        const data = dataDb.split('\n');
-        console.log(`Number of students: ${data.length - 1}`);
+/*
+* function to read database
+* @param {string} path
+* return {object}
+*/
 
-        for (let i = 1; i < data.length; i += 1) {
-          const line = data[i].split(','); // get each word
-          if (inFields[line[3]]) { // line[3] is the name field
-            inFields[line[3]].counter += 1;
-            inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-          } else {
-            inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
-          }
-        }
-        for (const key in inFields) {
-          if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-            console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-          }
-        }
-        resolve({ inFields, counter: data.length - 1 });
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
     });
-  };
-  return new Promise(promise);
+  });
 }
 
-module.exports = readDatabase
+export default readDatabase;
