50d2b4cf3a6831be4c0e0eb35e86313c9fc047a2
Yosef-S-A <yosefsamuel22@gmail.com>
2022-01-27 16:51:49 +0000

    jsdoc

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,3 +1,11 @@
+/*
+ * Basic test using Chai assertion library
+ * add, subtract or divide two numbers
+ * @author Yosef Samuel
+ * @param {number} a - first number
+ * @param {number} b - second number
+ * @return {number} - value after applying the operation
+ */
 function calculateNumber(type, a, b) {
   const aRound = Math.round(a);
   const bRound = Math.round(b);
